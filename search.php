1c05fd100963a5b68998c92e8d993a0f9ec1cd95
Balpreet Punia (surfy) <mebalpreetpunia@gmail.com>
2018-03-21 10:49:24 -0400

    Add files via upload

--- search.php
+++ search.php
@@ -4,8 +4,8 @@
 if (isset($_GET['term'])){
     $return_arr = array();
     try {
-        $dbh = new PDO("mysql:host=localhost;dbname=inventory", "root", "");
-        $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $dbh = new PDO("mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=acsm_03a9a5b692a77f1","b81a903bbbb0b7","606b3f2e");
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $stmt = $dbh->prepare('SELECT model FROM count WHERE model LIKE :term');
         $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
